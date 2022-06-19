7cb060c5104345fc2dc1335863deeafdd40c57ce
ease <easeanger@gmail.com>
2022-06-19 17:06:48 +0530

    rerendering hook when data change -testing

--- src/hooks/useFetch.test.js
+++ src/hooks/useFetch.test.js
@@ -4,9 +4,10 @@ import { renderHook } from "@testing-library/react-hooks";
 import { setupServer } from "msw/node";
 import { rest } from "msw";
 
-let URL = "https://jsonplaceholder.typicode.com/todos/";
+let TODO_URL = "https://jsonplaceholder.typicode.com/todos/";
+let POST_URL = "https://jsonplaceholder.typicode.com/posts";
 const server = setupServer(
-  rest.get(URL, (req, res, ctx) => {
+  rest.get(TODO_URL, (req, res, ctx) => {
     return res(
       ctx.json([
         {
@@ -23,8 +24,25 @@ const server = setupServer(
         },
       ])
     );
+  }),
+
+  rest.get(POST_URL, (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          body: `et iusto sed quo iure
+        voluptatem occaecati omnis eligendi aut ad
+        voluptatem doloribus vel accusantium quis pariatur
+        molestiae porro eius odio et labore et velit aut`,
+          id: 3,
+          title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+          userId: 1,
+        },
+      ])
+    );
   })
 );
+
 describe("useFetch", () => {
   ////starting server
   beforeAll(() => {
@@ -37,7 +55,9 @@ describe("useFetch", () => {
     server.close();
   });
   it("should fetch the data", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useFetch({ URL }));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useFetch({ URL: TODO_URL })
+    );
     await waitForNextUpdate();
     expect(result.current.data).toMatchInlineSnapshot(`
       Array [
@@ -52,6 +72,36 @@ describe("useFetch", () => {
           "id": 58,
           "title": "est dicta totam qui explicabo doloribus qui dignissimos",
           "userId": 3,
+        },
+      ]
+    `);
+  });
+
+  it("should fetch the data when TODO_URL changes", async () => {
+    const { result, waitForNextUpdate, rerender } = renderHook(
+      ({ URL }) => useFetch({ URL }),
+      {
+        //////what should be the initialProps
+        initialProps: {
+          URL: TODO_URL,
+        },
+      }
+    );
+    await waitForNextUpdate();
+    rerender({
+      URL: POST_URL,
+    });
+    await waitForNextUpdate();
+    expect(result.current.data).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "body": "et iusto sed quo iure
+              voluptatem occaecati omnis eligendi aut ad
+              voluptatem doloribus vel accusantium quis pariatur
+              molestiae porro eius odio et labore et velit aut",
+          "id": 3,
+          "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+          "userId": 1,
         },
       ]
     `);
